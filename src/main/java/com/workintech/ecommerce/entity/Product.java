c10007939e1d46cf6f725868a821e18ec0a0c142
Mehmet <mehmetsonmez216@gmail.com>
2024-01-08 02:02:51 +0300

    created spring boot project

---
+++ src/main/java/com/workintech/ecommerce/entity/Product.java
@@ -0,0 +1,32 @@
+package com.workintech.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "product",schema = "ecommerce")
+public class Product {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "image")
+    private Byte image;
+
+    @Column(name = "price")
+    private Double price;
+
+}
