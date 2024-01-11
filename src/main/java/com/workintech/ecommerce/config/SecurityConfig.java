2aad3818079c37571eb13a02bada264b517a4a22
Mehmet <mehmetsonmez216@gmail.com>
2024-01-11 13:02:04 +0300

    add SecurityConfig

---
+++ src/main/java/com/workintech/ecommerce/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.workintech.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+
+
+        return httpSecurity.csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(auth->{
+                  //  auth.requestMatchers("/category/**").permitAll();
+                    auth.requestMatchers("/product/**").permitAll();
+                    auth.anyRequest().authenticated();
+                }).formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+}
