3a1d942248405632702f1fd62f752e459bef9e0f
Mehmet <mehmetsonmez216@gmail.com>
2024-01-18 01:35:31 +0300

    I listed the products according to gender

--- src/main/java/com/workintech/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/workintech/ecommerce/repository/ProductRepository.java
@@ -1,7 +1,16 @@
 package com.workintech.ecommerce.repository;
 
+import com.workintech.ecommerce.entity.Enum.Gender;
 import com.workintech.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
+
+
+    @Query("SELECT p From Product p WHERE p.gender= :gender")
+    Product findProductByGender(long gender);
 }
