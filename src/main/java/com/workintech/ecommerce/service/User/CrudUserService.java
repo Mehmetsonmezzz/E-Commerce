d941d000459bebee9a634140b5540c5fbbed7c88
Mehmet <mehmetsonmez216@gmail.com>
2024-01-18 01:35:05 +0300

    Created crudUser service

---
+++ src/main/java/com/workintech/ecommerce/service/User/CrudUserService.java
@@ -0,0 +1,12 @@
+package com.workintech.ecommerce.service.User;
+
+import com.workintech.ecommerce.entity.User.ApplicationUser;
+
+public interface CrudUserService {
+
+
+
+    ApplicationUser saveAddress(ApplicationUser applicationUser);
+
+
+}
---
+++ src/main/java/com/workintech/ecommerce/service/User/CrudUserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.workintech.ecommerce.service.User;
+
+import com.workintech.ecommerce.entity.User.ApplicationUser;
+import com.workintech.ecommerce.repository.CityRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CrudUserServiceImpl implements CrudUserService{
+
+    @Override
+    public ApplicationUser saveAddress(ApplicationUser applicationUser) {
+        return null;
+    }
+}
